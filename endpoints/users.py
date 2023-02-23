bc422552e0928e9e937a3ed53680e656644cc9ed
agafurov <amir.a.gafurov@gmail.com>
2023-02-23 23:05:48 +0500

    adding utility fcns and schemas for basic user endpoints

--- endpoints/users.py
+++ endpoints/users.py
@@ -1,28 +1,32 @@
 from typing import Optional, List
-from fastapi import APIRouter
-from pydantic import BaseModel
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from data_base.db_setup import get_db
+from pydantic_schemas.user import User, UserCreate
+from endpoints.api_utils.users_utility import get_user, get_user_by_email, get_users, create_user
 
 router = APIRouter()
 
-list_of_users = []
-
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
-
 
 @router.get("/users", response_model = List[User])
-async def get_all_users():
-    return list_of_users
+async def read_all_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = get_users(db = db, skip = skip, limit = limit)
+    return users
 
 
-@router.post("/user")
-async def create_new_user(user: User):
-    list_of_users.append(user)
-    return "The user is successfully added."
+@router.post("/user", response_model = User, status_code = 201)
+async def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db = db, email = user.email)
+    if db_user:
+        raise HTTPException(status_code = 400, detail = "Email is already registered.")
+    return create_user(db = db, user = user)
 
 
-@router.get("/users/{id}")
-async def get_user_by_index(id: int):
-    return {"user": list_of_users[id]}
+@router.get("/users/{user_id}", response_model = User)
+async def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = get_user(db = db, user_id = user_id)
+    if db_user is None:
+        raise HTTPException(status_code = 404, detail = "User not found")
+    return db_user
